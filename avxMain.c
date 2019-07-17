aaf963ba67b7c405f8b89d126bf6fb847bb2b8ba
Lucia E <lesan2807@gmail.com>
2019-07-17 00:09:50 -0600

    di pos shunting yard funca, pero el algo todavia no hace las ops

--- avxMain.c
+++ avxMain.c
@@ -8,7 +8,7 @@ int main(int argc, char const *argv[])
 	double* points = (double*)calloc(100, sizeof(double)); 
 	char funcion[] = {"[0,1] [2,3] [4,5],x*x+x/300-22,0.1"};
 	extern void calcularPuntos(char*, double*, double, size_t, char*); 
-	calcularPuntos("0,1,2,3,4,5,0,0,0,0,0,0,x,x,*,x,300,/,+,22,-", points, 0.100000000000000000000000000000000000000, strlen(funcion), funcion); 
+	calcularPuntos("0,1,2,3,4,5,0,0,0,0,0,0,x,x,*,x,300,/,+,25,-,", points, 0.100000000000000000000000000000000000000, strlen(funcion), funcion); 
 	
 	return 0;
 }
--- vFunction.asm
+++ vFunction.asm
@@ -17,9 +17,16 @@ global calcularPuntos
 	    mov r14b, byte [%2]
 	    and r14b, 0x0F
 	    add r13, r14
-	    inc r12
+	    inc %2
 	    jmp %%getNumber
 
+%endmacro
+
+%macro getNumberFromStringMem 2
+		xor r10, r10 
+		mov r10, %2 
+		getNumberFromString %1, r10 
+		
 %endmacro
 
 %macro pushPila 0
@@ -137,7 +144,7 @@ global calcularPuntos
 	%%whileBeginEnd: 	
 		
 		forFillPoints %1, %2, %3, %4, %6, %7, %8  
-		; shuntingYard %5, %7, %8, operand1, operand2, result 
+		shuntingYard %5, %7, %8, operand1, operand2, result 
 
 		ucomisd xmm1, xmm2
 		jb %%whileBeginEnd
@@ -170,6 +177,7 @@ global calcularPuntos
 
 			movsd qword[%6+(r13*8)], xmm1 
 			addsd xmm1, xmm0 
+			jmp %%finForFill8
 
 			%%putCeroX: 
 				xorpd xmm6, xmm6
@@ -186,18 +194,151 @@ global calcularPuntos
 			movsd qword[%7+(r13*8)], xmm3 
 
 			addsd xmm1, xmm0 
-			addsd xmm3, xmm0  			
+			addsd xmm3, xmm0
+			jmp %%finForFill8
 
 			%%putCeroXandY: 
 				xorpd xmm6, xmm6 
 				xorpd xmm7, xmm7 
 				movsd qword[%6+(r13*8)], xmm6
 				movsd qword[%7+(r13*8)], xmm7 
+
 	
 	%%finForFill8: 
 		inc r13 	
 		cmp r13, 8  
 		jl %%forFill8  
+
+%endmacro
+
+; %1 r12 ; donde está la función  
+; %2 pointsX
+; %3 pointsY
+; %4 operand1 
+; %5 operand2 
+; %6 result
+%macro shuntingYard 6
+	
+	%%shuntingYardBegin:
+		xor r15, r15 	
+		xor r14, r14 
+
+	; for each token 
+	%%checkIfToken:  
+		; limpiar el token 
+
+	%%cleanToken:
+		mov byte[charToken+r14], '-' 
+		inc r14 
+		cmp r14, 10
+		jl %%cleanToken 	
+
+		xor r14, r14 
+		cmp byte[%1], ','
+		je %%checkWhatToDo
+		mov r14b, byte[%1] 
+		mov [charToken+r15], r14b
+		inc r15
+		inc %1 
+		jmp %%checkIfToken
+
+	%%checkWhatToDo:
+		; switch gigante de si es operador o si es operando
+		cmp byte[charToken], 'x'
+		je %%itIsAnX
+
+		cmp byte[charToken], 'y'
+		je %%itIsAnY
+
+		cmp byte[charToken], 's'
+		je %%checkSen 
+
+		cmp byte[charToken], 'c'
+		je %%checkCos
+
+		cmp byte[charToken], 'l'
+		je %%checkLn 
+
+		cmp byte[charToken], '+'
+		je %%itIsSum
+		
+		cmp byte[charToken], '-'
+		je %%itIsResta
+
+		cmp byte[charToken], '*'
+		je %%itIsMul 
+
+		cmp byte[charToken], '/'
+		je %%itIsDiv  
+
+	%%getNumber:
+		xor r13, r13	
+		getNumberFromStringMem %%broadcastNum, charToken
+		%%broadcastNum: 
+			mov qword[number], r13 
+			pushNumberToStack number
+		jmp %%endOfShuntingYard
+
+	%%itIsDiv: 
+		jmp %%endOfShuntingYard
+	%%itIsMul: 
+		jmp %%endOfShuntingYard
+	%%itIsResta: 
+		jmp %%endOfShuntingYard
+	%%itIsSum: 
+		jmp %%endOfShuntingYard
+	%%itIsAnX: 
+		jmp %%endOfShuntingYard
+	%%itIsAnY: 
+		jmp %%endOfShuntingYard
+	%%checkLn: 
+		jmp %%endOfShuntingYard
+	%%checkCos: 
+		jmp %%endOfShuntingYard
+	%%checkSen: 
+		jmp %%endOfShuntingYard
+	%%endOfShuntingYard:
+		inc %1 
+		cmp byte[%1], 0
+		jne %%shuntingYardBegin	
+
+%endmacro
+
+; %1 memory or register to push 
+%macro pushToStack 1
+		
+	mov r15, 7 
+	
+	%%pushOperand: 
+		push qword[%1+r15*8]
+		dec r15 
+		cmp r15, -1 
+		jg %%pushOperand		
+
+%endmacro 
+
+%macro pushNumberToStack 1
+		
+	mov r15, 7 
+	
+	%%pushOperand: 
+		push qword[%1]
+		dec r15 
+		cmp r15, -1 
+		jg %%pushOperand		
+
+%endmacro 
+
+
+; %1 memory or register to pop  
+%macro popOperandToStack 1
+	
+	xor r15, r15 
+	%%popOperand: 
+		pop qword[%1+r15*8]
+		inc r15 
+		cmp r15, 8
+		jl %%popOperand 
 
 
 %endmacro
@@ -215,6 +356,10 @@ endY dq 0
 
 pointsX dq 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
 pointsY dq 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0  
+
+charToken db "----------" , 0 
+
+number dq 0 
 
 section .text
 
